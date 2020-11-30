8d12807ebd3f706483e25d2ec9435eb087cf13fe
DAW2-Vicente21 <71894608+DAW2-Vicente21@users.noreply.github.com>
2020-11-30 03:21:07 +0100

    Create relationship.php

---
+++ relationship.php
@@ -0,0 +1,13 @@
+<?php
+$db = mysqli_connect(gethostname(), 'root', 'root') or 
+    die ('Unable to connect. Check your connection parameters.');
+mysqli_select_db($db,'moviesite') or die(mysqli_error($db));
+
+$query = 'ALTER TABLE movie 
+    ADD CONSTRAINT FK_movieActor
+    FOREIGN KEY (movie_leadActor) REFERENCES people(people_id)';
+
+
+echo 'Added relation beetwen tables!'
+
+?>
